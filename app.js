2e9214afa3752d19eeee14b9e4e8b8a2750066fd
Chaithan-Reddy <chaithanreddy99@gmail.com>
2025-12-31 10:51:06 +0530

    updated version2.0

--- app.js
+++ app.js
@@ -5,20 +5,56 @@ readjs = document.getElementById('jscode');
 const runbtn = document.getElementById("runbutton");
 const outputFrame = document.getElementById('output');
 
+function runCode() {
+    const run_html = readhtml.value;
+    const run_css = readcss.value;
+    const run_js = readjs.value;
 
-runbtn.addEventListener('click',() => {
-    run_html = readhtml.value
-    run_css = readcss.value
-    run_js = readjs.value
-    combined_content = 
+    const combined_content =
     "<html>" +
     "<head>" +
-    "<style>" + run_css + "</style>" +
+      "<style>" +
+        run_css +
+        "#error{color:red;font-family:monospace;padding:10px;}" +
+      "</style>" +
     "</head>" +
     "<body>" +
-    run_html +
+
+      "<div id='error'></div>" +
+      run_html +
+
+      "<script>" +
+        "window.onerror = function(message, source, line) {" +
+          "document.getElementById('error').innerText =" +
+          "'Error: ' + message + ' (line ' + line + ')';" +
+        "};" +
+      "<\/script>" +
+
+      "<script>" +
+        run_js +
+      "<\/script>" +
+
     "</body>" +
-    "<script>" + run_js + "<\/script>"
-    "</html>"
-    outputFrame.srcdoc = combined_content
-})
+    "</html>";
+
+    outputFrame.srcdoc = combined_content;
+   // storing data in local storage.
+
+    localStorage.setItem("htmlcode", run_html);
+    localStorage.setItem("csscode", run_css);
+    localStorage.setItem("jscode", run_js); 
+}
+
+
+
+// reading data after refreshing page.
+readhtml.value = localStorage.getItem("htmlcode") || "";
+readcss.value  = localStorage.getItem("csscode") || "";
+readjs.value   = localStorage.getItem("jscode") || "";
+runCode()
+
+runbtn.addEventListener('click',runCode);
+readhtml.addEventListener("input",runCode);
+readcss.addEventListener("input",runCode);
+readjs.addEventListener("input",runCode);
+
